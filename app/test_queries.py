6f3d096325b76b7c21c682196bd78e51bff3fac8
Debarpita Chakraborty <165559859+debarpita14@users.noreply.github.com>
2025-08-10 09:38:36 +0530

    Update test_queries.py

--- app/test_queries.py
+++ app/test_queries.py
@@ -1,3 +1,13 @@
+from queries import get_kda_winrate
+
+df = get_kda_winrate()
+print(df.head())
+
+from queries import get_top_3_victims
+
+df = get_top_3_victims()
+print(df.head())
+
 from queries import get_damage_stats
 
 df = get_damage_stats()
